261f8d611f8101440b5c59ac40791940eefd2f25
kash2005 <kashmifernando3@gmail.com>
2023-10-10 21:24:07 +0530

    add forms

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,66 @@ let load = 0;
 
 let int = setInterval(blurring, 30); // 30ms
 
+window.addEventListener('load', ()=>{
+    $('#navbar-nav>a').eq(0).css("color", "#435334" );
+    $('#navbar-nav>a').eq(1).css("color", "#94A686" );
+    $('#navbar-nav>a').eq(2).css("color", "#94A686" );
+});
+
 const clear = () => {
-    const loadingScreen = document.getElementById('sahan');
-    loadingScreen.style.display='block';
+    const loadingScreen = document.querySelector('#sahan');
+    loadingScreen.style.display = 'block';
+
+    const loadingScreen1 = document.querySelector('.customerManage');
+    loadingScreen1.style.display = 'none';
+
+    const loadingScreen2 = document.querySelector('.itemManage');
+    loadingScreen2.style.display = 'none';
+    const loadingScreen3 = document.querySelector('.header');
+    loadingScreen3.style.display = 'block';
+
+}
+
+const clear1 = () => {
+    const loadingScreen = document.querySelector('#sahan');
+    loadingScreen.style.display = 'none';
+
+    const loadingScreen1 = document.querySelector('.customerManage');
+    loadingScreen1.style.display = 'block';
+
+    const loadingScreen2 = document.querySelector('.itemManage');
+    loadingScreen2.style.display = 'none';
+    const loadingScreen3 = document.querySelector('.header');
+    loadingScreen3.style.display = 'block';
+
+}
+
+const clear2 = () => {
+    const loadingScreen = document.querySelector('#sahan');
+    loadingScreen.style.display = 'none';
+
+    const loadingScreen1 = document.querySelector('.customerManage');
+    loadingScreen1.style.display = 'none';
+
+    const loadingScreen2 = document.querySelector('.itemManage');
+    loadingScreen2.style.display = 'block';
+    const loadingScreen3 = document.querySelector('.header');
+    loadingScreen3.style.display = 'block';
+
 }
 
 function blurring() {
     load++
-    const loadingScreen = document.getElementById('sahan');
-    loadingScreen.style.display='none';
+    const loadingScreen = document.querySelector('#sahan');
+    loadingScreen.style.display = 'none';
+
+    const loadingScreen1 = document.querySelector('.customerManage');
+    loadingScreen1.style.display = 'none';
+
+    const loadingScreen2 = document.querySelector('.itemManage');
+    loadingScreen2.style.display = 'none';
+    const loadingScreen3 = document.querySelector('.header');
+    loadingScreen3.style.display = 'none';
     if(load > 99) {
         clearInterval(int)
             const loadingScreen = document.getElementById('kasmi');
@@ -27,6 +78,28 @@ function blurring() {
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 
 }
+
+$('#navbar-nav>a').eq(0).on('click', function (){
+    clear();
+
+    $('#navbar-nav>a').eq(0).css("color", "#435334" );
+    $('#navbar-nav>a').eq(1).css("color", "#94A686" );
+    $('#navbar-nav>a').eq(2).css("color", "#94A686" );
+})
+
+$('#navbar-nav>a').eq(1).on('click', function (){
+    clear1();
+    $('#navbar-nav>a').eq(1).css("color", "#435334" );
+    $('#navbar-nav>a').eq(0).css("color", "#94A686" );
+    $('#navbar-nav>a').eq(2).css("color", "#94A686" );
+})
+
+$('#navbar-nav>a').eq(2).on('click', function (){
+    clear2();
+    $('#navbar-nav>a').eq(1).css("color", "#94A686" );
+    $('#navbar-nav>a').eq(0).css("color", "#94A686" );
+    $('#navbar-nav>a').eq(2).css("color", "#435334" );
+})
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
